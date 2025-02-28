7c18e61161a67234de8df094c836bc81b5f44874
Francesco Vito Giotta <167875380+LemonPower21@users.noreply.github.com>
2025-02-28 21:09:33 +0100

    Add files via upload

---
+++ Partisan.py
@@ -0,0 +1,50 @@
+from scapy.all import sniff, IP, TCP, UDP, Ether
+import os
+import platform
+
+count = 0
+def clean():
+    os.system('cls' if platform.system() == 'Windows' else 'clear')
+def packets(packet):
+    global count
+    count = count + 1 
+    if IP in packet:
+        ipsource = packet[IP].src
+        ipdestination = packet[IP].dst
+        protocol = packet[IP].proto
+        ttl = packet[IP].ttl
+        iplength = packet[IP].len
+        if Ether in packet:
+            macsource = packet[Ether].src
+            macdestination = packet[Ether].dst
+
+        if TCP in packet:
+            tcpsource = packet[TCP].sport
+            tcpdestination = packet[TCP].dport
+            tcpflags = packet[TCP].flags
+            tcpsequence = packet[TCP].seq
+            tcpacknowledgment = packet[TCP].ack
+            tcppayload = packet[TCP].payload.original if packet[TCP].payload else "No Payload"
+            tcppayloadlength = len(packet[TCP].payload.original) if packet[TCP].payload else 0
+	
+            print(f"Packet Number: {count}")
+            print(f"Source MAC: {macsource}\nDestination MAC: {macdestination}")
+            print(f"Source SOCKET: {ipsource}:{tcpsource}\nDestination SOCKET: {ipdestination}:{tcpdestination}")
+            print(f"Protocol: TCP\nFlags: {tcpflags}\nSequence: {tcpsequence}\nACK: {tcpacknowledgment}\nPayload Length: {tcppayloadlength}\nTTL: {ttl}\nIP Length: {iplength}\n")
+
+        elif UDP in packet:
+            udpsource = packet[UDP].sport
+            udpdestination = packet[UDP].dport
+            udppayload = packet[UDP].payload.original if packet[UDP].payload else "No Payload"
+            udppayloadlength = len(packet[UDP].payload.original) if packet[UDP].payload else 0
+            print(f"Packet Number: {count}")
+            print(f"Source MAC: {macsource}\nDestination MAC: {macdestination}")
+            print(f"Source SOCKET: {ipsource}:{udpsource}\nDestination SOCKET: {ipdestination}:{udpdestination}")
+            print(f"Protocol: UDP\nPayload Length: {udppayloadlength}\nTTL: {ttl}\nIP Length: {iplength}\n")
+
+def main():
+    clean()
+    print("Sniffing...\n\n")
+    sniff(prn=packets, store=0)
+
+main()
